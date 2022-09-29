647ec48585f4fce4780a4d79d2f67b506a428c57
Ramya2001bca <113408822+Ramya2001bca@users.noreply.github.com>
2022-09-29 09:49:22 +0530

    Add files via upload

---
+++ calculator1.java
@@ -0,0 +1,26 @@
+class calculator1{
+
+
+int add(int a,int b){
+                int c=a+b;
+                System.out.println(c);
+                 return c;
+}
+int sub(int a,int b){
+                  int c=a-b;
+                  
+                  System.out.println(c);
+                    return c;
+                    }
+int mul(int a,int b){
+                int c=a*b;
+                System.out.println(c);
+                    return c;
+}
+int div(int a,int b){
+            int c=a%b;
+            System.out.println(c);
+            return c;
+            }
+}
+
---
+++ shap.java
@@ -0,0 +1,10 @@
+class shap {
+public static void main(String[] args){ 
+calculator1 ramya7=new calculator1();
+ramya7.add(6,7);
+ramya7.sub(8,5);
+ramya7.mul(10,10);
+ramya7.div(2,5);
+}
+}
+
